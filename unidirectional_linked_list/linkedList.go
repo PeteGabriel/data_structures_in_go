0c83b1d99c7741e684d4c1261957c0f98c9fe32c
PeteGabriel <pedrogabriel@protonmail.ch>
2020-07-15 10:15:53 +0200

    First push

--- unidirectional_linked_list/linkedList.go
+++ unidirectional_linked_list/linkedList.go
@@ -12,3 +12,15 @@ type Node struct {
 func New() *List {
 	return &List{}
 }
+
+func (l *List) AddData(data string){
+	n := &Node{
+		data:data,
+		next:nil,
+	}
+	if l.node == nil {
+		l.node = n
+	}else {
+		l.node.next = n
+	}
+}
--- unidirectional_linked_list/linkedList_test.go
+++ unidirectional_linked_list/linkedList_test.go
@@ -11,3 +11,10 @@ func Test(t *testing.T) {
 	l := New()
 	is.True(l != nil)
 }
+
+func Test_AddData(t *testing.T) {
+	is := is.New(t)
+	l := New()
+	l.AddData("New Node Data")
+	is.True(l.node.data == "New Node Data")
+}
