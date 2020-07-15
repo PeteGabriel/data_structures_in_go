ba388bf83f59b4fb401a905c069c6e5e46a6070a
PeteGabriel <pedrogabriel@protonmail.ch>
2020-07-15 11:52:46 +0200

    Add tests and new Insert operation

--- unidirectional_linked_list/linkedList.go
+++ unidirectional_linked_list/linkedList.go
@@ -2,6 +2,7 @@ package unidirectional_linked_list
 
 type List struct{
 	node* Node
+	len int
 }
 
 type Node struct {
@@ -13,6 +14,13 @@ func New() *List {
 	return &List{}
 }
 
+func (l *List) size() int {
+	return l.len
+}
+
+/**
+ Add new data.
+ */
 func (l *List) AddData(data string){
 	n := &Node{
 		data:data,
@@ -22,5 +30,33 @@ func (l *List) AddData(data string){
 		l.node = n
 	}else {
 		l.node.next = n
+	}
+
+	l.len += 1
+}
+
+/**
+ Insert new data at given position.
+ If given position is bigger than current size, just add to front of list.
+*/
+func (l *List) Insert(pos int, data string){
+	if pos > l.size() {
+		l.AddData(data)
+		return
+	}
+
+	n := &Node{
+		data:data,
+		next:nil,
+	}
+
+	i := 1
+	for l.node.next != nil {
+		if i == (pos-1) {
+			n.next = l.node.next
+			l.node.next = n
+			l.len += 1
+			return
+		}
 	}
 }
--- unidirectional_linked_list/linkedList_test.go
+++ unidirectional_linked_list/linkedList_test.go
@@ -18,3 +18,25 @@ func Test_AddData(t *testing.T) {
 	l.AddData("New Node Data")
 	is.True(l.node.data == "New Node Data")
 }
+
+func Test_AddDataToAnExistentList(t *testing.T) {
+	is := is.New(t)
+	l := New()
+	l.AddData("First Node")
+	l.AddData("Second Node")
+
+	is.True(l.node.data == "First Node")
+	is.True(l.node.next.data == "Second Node")
+}
+
+func Test_InsertAtPositionX(t *testing.T) {
+	is := is.New(t)
+	l := New()
+	l.AddData("First Node")
+	l.AddData("Second Node")
+	l.Insert(2, "New Second Node")
+
+	is.True(l.node.data == "First Node")
+	is.True(l.node.next.data == "New Second Node")
+	is.True(l.node.next.next.data == "Second Node")
+}
